d115ae489b1b71e915561acbd534f33f5d1cea8a
Emmanuel WALDNER <emmanuel.waldner@gmail.com>
2021-06-06 22:23:30 +0200

    Activation menu et nettoyage

--- optimized.py
+++ optimized.py
@@ -2,6 +2,9 @@
 import csv
 import os
 import time
+from math import ceil
+
+import logging
 
 data_folder = "data/"
 
@@ -18,16 +21,16 @@ class Data:
         """
         Methode qui va cherche le fichier à analyser
         """
-        # print('Saisir le nom du fichier à analyser [action.csv si vide][1 = dataset1_Python+P7.csv][2 = '
-        #       'dataset1_Python+P7.csv]:')
-        # file = input('---->')
-        # if file == '':
-        # file = 'action.csv'
-        # elif file == "1":
-        #     file = 'dataset1_Python+P7.csv'
-        # elif file == "2":
-        #     file = 'dataset2_Python+P7.csv'
-        file = 'action.csv'
+        print('Saisir le nom du fichier à analyser [action.csv si vide][1 = dataset1_Python+P7.csv][2 = '
+              'dataset2_Python+P7.csv]:')
+        file = input('---->')
+        if file == '':
+            file = 'action.csv'
+        elif file == "1":
+            file = 'dataset1_Python+P7.csv'
+        elif file == "2":
+            file = 'dataset2_Python+P7.csv'
+
         file_to_open = data_folder + file
 
         if os.path.exists(file_to_open):
@@ -43,6 +46,7 @@ class Data:
                         pass
         else:
             print("Le fichier n'existe pas")
+            return False
 
         return self.actions
 
@@ -69,28 +73,29 @@ class Action:
         """
         self.name = args[0]
         self.value = float(args[1])
+        self.value2 = ceil(self.value)
         self.taux = float(str(args[2]).replace('%', ''))
         self.profit = round(self.value * self.taux / 100, 2)
 
     def __repr__(self):
         return "{}".format(self.name)
 
-    def __lt__(self, other):
-        return self.taux < other.taux
-
-    def __gt__(self, other):
-        return self.taux > other.taux
-
 
 def optimized(data: list, investissement: float):
+    gestion_cents = False
+    if gestion_cents:
+        facteur = int(100)
+    else:
+        facteur = int(1)
 
-    investissement = int(investissement * 100)
+    investissement = int(investissement * facteur)
 
     matrice = [[0 for x in range(investissement + 1)] for x in range(len(data) + 1)]
 
-    for i in range(1, len(data) + 1):  # parcours des actions
-        for w in range(1, investissement + 1):  # parcours du montant a investir (*100 pour gérer les centimes)
-            valeur_action = int(data[i - 1].value * 100)
+    for w in range(1, investissement + 1):
+        for i in range(1, len(data) + 1):  # parcours des actions
+            # parcours du montant a investir (*100 pour gérer les centimes)
+            valeur_action = int(data[i - 1].value2 * facteur)
             if valeur_action <= w:  # si il me reste des fonds pour investir/acheter l'action je
                 # verifie si j'augmente mon profit en achetant celle ci.
                 matrice[i][w] = max(data[i - 1].profit + matrice[i - 1][int(w - valeur_action)], matrice[i - 1][w])
@@ -103,11 +108,11 @@ def optimized(data: list, investissement: float):
     data_selection = []
 
     while w >= 0 and n >= 0:
-        v = int(data[n - 1].value * 100)  # valeur de l'action * 100 pour les centimes.
+        valeur_action = int(data[n - 1].value2 * facteur)  # valeur de l'action * 100 pour les centimes.
         d = data[n - 1].profit  # Valeur du profit
-        if matrice[n][w] == matrice[n - 1][w - v] + d:
+        if matrice[n][w] == matrice[n - 1][w - valeur_action] + d:
             data_selection.append(data[n - 1])
-            w -= v
+            w -= valeur_action
         n -= 1
 
     t = sum([v.value for v in data_selection])
@@ -147,26 +152,22 @@ def affichage(data, data_time=None):
     print('------------------------ Optimized ------------------------')
     print('------------------------------------------------------------')
 
-    print('La meilleur proposition est la suivante : {0}\nAvec une dépense totale de : {1:.2f} \nAvec un profit '
-          'espéré de : {2:.2f}'.format(data[0], data[1], data[2]))
+    print('La meilleur proposition est la suivante :')
+    print(*data[0], sep="\n")
+    print('Avec une dépense totale de : {0:.2f} \nAvec un profit '
+          'espéré de : {1:.2f}'.format(data[1], data[2]))
     print('------------------------------------------------------------')
     print('Calculé en {0:.2f} secondes'.format(data_time))
 
 
 def main():
     action = Data().load_actions()
-    start = time.time()
-    proposition = optimized(action, 500)
-    stop = time.time()
-    duree = stop - start
-    print(proposition)
-    affichage(proposition, duree)
+    if action:
+        start = time.time()
+        proposition = optimized(action, 500)
+        stop = time.time()
+        duree = stop - start
+        affichage(proposition, duree)
 
-    start = time.time()
-    proposition = glouton(action, 500)
-    stop = time.time()
-    duree = stop - start
-    print(proposition)
-    affichage(proposition, duree)
 
 main()
